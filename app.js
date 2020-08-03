d1640d777099a22cb5e25cdd450e9e9152fb7980
Pieter <42219172+PidO1@users.noreply.github.com>
2020-08-03 20:15:37 +0200

    First Graphql Call Added

--- app.js
+++ app.js
@@ -1,14 +1,52 @@
 const express = require('express');
 const bParser = require('body-parser');
 const dotenv = require('dotenv');
+const {graphqlHTTP}  = require('express-graphql')
+const {buildSchema, } = require('graphql');
 
 //Initialize needed configs for server
 const app = express();
-dotenv.config();
+app.use(bParser.json());
+app.use('/graphql', graphqlHTTP({
 
-app.get('/',(req,res,next)=>{
+        schema: buildSchema(`
+                type RootQuery{
+                        events : [String!]!
 
-        res.send('hello world!');
-});
 
-app.listen(process.env.PORT);
+                }
+                type RootMutation {
+                        createEvent(name: String!):String
+
+
+                }
+                schema{
+                        query: RootQuery
+                        mutation: RootMutation
+
+                }
+        `),
+        rootValue:{
+                events: ()=>{
+                        return ['well', 'so','it']
+                },
+                createEvent: (args)=>{
+                        let eventName = args.name;
+
+                        return eventName;
+
+                }
+        
+        
+        },
+        graphiql : true
+
+
+})
+
+);
+
+app.listen(4000);
+console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+
+
